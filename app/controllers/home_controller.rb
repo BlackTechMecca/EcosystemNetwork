dc6364ef79ac83b36f58a45cf47f1bcb76b9a4fd
Myron Hicks <myron.hicks90@gmail.com>
2016-01-06 15:08:12 -0600

    landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,37 @@
+require 'mailchimp'
+
 class HomeController < ApplicationController
+	protect_from_forgery with: :null_session
+
+  layout "home"
+
+  before_action :setup_mcapi
+
+	def setup_mcapi
+  		@mc = Mailchimp::API.new('b023f41df72436528a3f4edaa604b237-us11')
+  end
+
   def index
   end
+
+  def subscribe
+  	newsletter_id = '71480d5182'
+  	email = params['email']
+  	begin
+				@mc.lists.subscribe(newsletter_id, {'email' => email})
+			rescue Mailchimp::ListDoesNotExistError
+				flash[:danger] = "Oops. Something went wrong. Please try again later."
+				render '/home/index'
+			return
+			rescue Mailchimp::Error => ex
+			if ex.message
+				flash[:danger] = ex.message
+			else
+				flash[:danger] = "Oops. Something went wrong. Please try again later."
+			end
+		end
+			flash[:success] = "Thank you, your sign-up request was successful! Please check your e-mail inbox."
+			render '/home/index'
+  end
+
 end
