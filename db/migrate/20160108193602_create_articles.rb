9cef8b55f24b4cfa829a8cab5a2fdbe5f01ee068
Andrew Carr <aacarr5@gmail.com>
2016-01-08 14:44:31 -0600

    Create article migration

--- db/migrate/20160108193602_create_articles.rb
+++ db/migrate/20160108193602_create_articles.rb
@@ -2,6 +2,10 @@ class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
 
+      t.string :title, null:false
+      t.text :body, null:false
+      t.string :img_url
+
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107043055) do
+ActiveRecord::Schema.define(version: 20160108193602) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.text     "body",       null: false
+    t.string   "img_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "full_profiles", force: :cascade do |t|
     t.datetime "last_modified_timestamp"
