470376606ae1d3680125130cb053fb7b32423551
Andrew Carr <aacarr5@gmail.com>
2016-01-08 17:02:59 -0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108204627) do
+ActiveRecord::Schema.define(version: 20160108215106) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      null: false
@@ -51,6 +51,19 @@ ActiveRecord::Schema.define(version: 20160108204627) do
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id"
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "article_id", null: false
+    t.integer  "tag_id",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
