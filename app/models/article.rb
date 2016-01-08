218d12ce1f154b43839276349f5a9085272c036e
Andrew Carr <aacarr5@gmail.com>
2016-01-08 17:00:54 -0600

    Create taggins relationship

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
 
 	has_many :comments
+	has_many :taggings
 
 	validates :title, presence: true, length: { minimum: 6}
 	validates :body, presence: true, length: {minimum: 400}
