21a06bc2f2f876f64b748d7c1e8011baa43feb3d
Andrew Carr <aacarr5@gmail.com>
2016-01-08 14:44:56 -0600

    Create article specs

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Article, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	let(:article) {FactoryGirl.create(:article)}
+	let(:empty_article) {FactoryGirl.build(:article,title:nil,body:"asdfasdfasdfasdfasdf",img_url:nil)}
+
+	describe "#initialize" do
+
+		it "should have a title" do
+			expect(article.title).to be_a String
+			expect(empty_article.valid?).to be false
+		end
+
+		it 'should have a body' do 
+			expect(article.body.length).to be >= 200
+			expect(empty_article.valid?).to be false
+		end
+
+		it 'has an optional image tag' do
+			expect(article.img_url).to be_a String
+		end
+	end
+  
 end
+
