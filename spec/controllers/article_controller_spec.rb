33d82134c8b5da7c8320e06836dbdc599b9adb1a
Andrew Carr <aacarr5@gmail.com>
2016-01-09 14:20:38 -0600

    Create basic tests  for index page

--- spec/controllers/article_controller_spec.rb
+++ spec/controllers/article_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe ArticleController, :type => :controller do
 
+	describe "#index" do
+		it "renders the index template properly" do
+			get :index
+			expect(response.status).to render_template "index"
+			expect(response.status).to be 200 
+		end 
+
+		it 'renders all the articles' do 
+			get :index
+			10.times {FactoryGirl.create(:article)}
+		end
+	end
+
 end
