95d19cb4e96a5e0e5c0860dd142e36d657368023
Andrew Carr <aacarr5@gmail.com>
2016-01-09 13:07:38 -0600

    Create assoications

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,16 @@
+require 'pry'
+
 class Article < ActiveRecord::Base
 
 	has_many :comments
 	has_many :taggings
+	has_many :tags, through: "tagging s", :source => "tag"
 
 	validates :title, presence: true, length: { minimum: 6}
-	validates :body, presence: true, length: {minimum: 400}
+	validates :body, presence: true, length: {minimum: 100}
+
+	# def tag(tag_list)
+	# 	tag_list.each {|tag| Tagging.create(article:self,tag:tag)}
+	# end
 
 end
