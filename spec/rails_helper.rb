7560df3ae7c7d226f775037ee4c8696d0bc7facd
Chrissie <chrissie.deist@gmail.com>
2016-01-24 16:02:43 -0600

    Add pry to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
 require 'devise'
+require 'pry'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
