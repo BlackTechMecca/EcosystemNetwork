bbbcd51b72dc34e6362f47dba9c524942d08fe19
Andrew Carr <aacarr5@gmail.com>
2016-01-26 17:02:43 -0600

    Users page says 'submit an article'

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -7,9 +7,14 @@ RSpec.describe "users/show.html.erb", :type => :view do
   		assign(:user, FactoryGirl.create(:user,first_name:"Andrew"))
   	end
 
-  	it "should have a form to create an article" do 
+  	it "should display the users name" do 
   		render
   		expect(rendered).to match(/Andrew/)
+  	end
+
+  	it 'should contain a form to submit an article' do 
+  		render
+  		expect(rendered).to match(/submit an article/i)
   	end
 
 
