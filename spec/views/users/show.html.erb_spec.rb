56ae2d6676e0a8bb92cb1e1444f0e3701b15680c
Andrew Carr <aacarr5@gmail.com>
2016-01-26 16:57:36 -0600

    Create user#show view  spec

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "users/show.html.erb", :type => :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "The user home page" do 
+
+  	before(:each) do 
+  		assign(:user, FactoryGirl.create(:user,first_name:"Andrew"))
+  	end
+
+  	it "should have a form to create an article" do 
+  		render
+  		expect(rendered).to match(/Andrew/)
+  	end
+
+
+
+  end
+
+
+
 end
