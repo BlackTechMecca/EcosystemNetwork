798e953f3f04d79d55b53878b8b69a634b9a871e
Andrew Carr <aacarr5@gmail.com>
2016-01-26 18:36:33 -0600

    Creting form for article

--- spec/controllers/article_controller_spec.rb
+++ spec/controllers/article_controller_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe ArticleController, :type => :controller do
-
+	login_user # => what does this do? (AC)
 	describe "#index" do
 		it "renders the index template properly" do
 			get :index
 			expect(response.status).to render_template "index"
 			expect(response.status).to be 200 
 		end 
+	end
+
+	let(:user) {FactoryGirl.create(:user)}
+	let(:valid_attributes) {FactoryGirl.create(:article,user:user)}
+
+	describe "PUT" do 
+		
 	end
 
 end
