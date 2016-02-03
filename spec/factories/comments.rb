33f488e77fa107483a943dc645060cf36842efeb
Andrew Carr <aacarr5@gmail.com>
2016-02-02 18:45:24 -0600

    Change

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,7 +2,8 @@ FactoryGirl.define do
   factory :comment do
   	user             { FactoryGirl.create(:user) }
   	body    	     { Faker::Lorem.sentence }
-  	commentable_type { ["Article", "JobPost"].sample }
+  	commentable_type {FactoryGirl.create}
+  	# commentable_type { ["Article", "JobPost"].sample }
   	commentable_id   {1}
     
   end
