b85843157a016b379d83d75631755e8902992756
Andrew Carr <aacarr5@gmail.com>
2016-02-02 19:09:51 -0600

    Update facotry girl with transient attribute

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,11 +1,13 @@
 FactoryGirl.define do
   factory :comment do
+  	transient do 
+  		commentable nil
+  	end
   	user             { FactoryGirl.create(:user) }
   	body    	     { Faker::Lorem.sentence }
-  	commentable_type {FactoryGirl.create}
-  	# commentable_type { ["Article", "JobPost"].sample }
-  	commentable_id   {1}
-    
+  	commentable_type { commentable.class }
+  	commentable_id  { commentable.id }
+ 
   end
 
 end
